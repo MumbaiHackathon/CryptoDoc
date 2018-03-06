dd303040bde96681dd830d49d053b4ee0f92100d
Yash Puthran <puthranyash1@gmail.com>
2018-03-07 03:30:47 +0530

    Added v3 of CryptoDoc

--- contracts/CryptoDoc.sol
+++ contracts/CryptoDoc.sol
@@ -6,6 +6,11 @@ contract Profile {
     string public name;
     string public nationality;
     address public aadharData;
+    address public addressData;
+    address public mobileData;
+    
+    //serviceProviders
+    mapping (address => bool) public mobProviders;
 
     string public aadharStatus;
     string public passportStatus;
@@ -18,27 +23,72 @@ contract Profile {
         _;
     }
     
+    modifier onlyMob() {
+        require(mobProviders[msg.sender]);
+        _;
+    }
+    
     function Profile(string _name, string _nation) public {
         name = _name;
         nationality = _nation;
         manager = msg.sender;
+        //initializeServices();
     }
     
-    function createAadhar(address _profile, address _userAddress, uint _aadharNumber, uint _mobile) public restricted {
-       aadharData =  new Aadhar(_profile, _userAddress, _aadharNumber, _mobile);
+    function initializeServices() public {
+        mobProviders[msg.sender] = true;
+    }
+        
+    function createAddress(address _profile, string _local, string _city, string _state, string _nation) public restricted {
+        addressData = new Address(_profile, _local, _city, _state, _nation);
+    }
+    
+    function createMobile(address _profile, uint _number) public onlyMob {
+        mobileData = new Mobile(_profile, _number, msg.sender);
+    }
+    
+    function createAadhar(address _profile, address _userAddress,  address _mobile, uint _aadharNumber) public restricted {
+       aadharData =  new Aadhar(_profile, _userAddress,  _mobile, _aadharNumber);
        aadharStatus = "verified";
     }
+}
+
+contract Address {
+    address public profile;
+    string private local;
+    string private city;
+    string private state;
+    string private nation;
     
+    function Address(address _profile, string _local, string _city, string _state, string _nation) public {
+        profile = _profile;
+        local = _local;
+        city = _city;
+        state = _state;
+        nation = _nation;
+    }
+}
+
+contract Mobile {
+    address public profile;
+    address public serviceProvider;
+    uint public number;
+    
+    function Mobile(address _profile, uint _number, address _serviceProvider) public {
+        serviceProvider = _serviceProvider;
+        profile = _profile;
+        number = _number;
+    }
 }
 
 contract Aadhar {
     address public profile;
     address public userAddress;
+    address public mobile;
     uint public aadharNumber;
-    uint public mobile;
     address public placeholder;
         
-    function Aadhar(address _profile, address _userAddress, uint _aadharNumber, uint _mobile) public {
+    function Aadhar(address _profile, address _userAddress,  address _mobile, uint _aadharNumber) public {
         profile = _profile;
         userAddress = _userAddress;
         aadharNumber = _aadharNumber;
@@ -46,6 +96,5 @@ contract Aadhar {
         placeholder = this;
     }
 }
-
 
 
