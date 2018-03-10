3f01b69340971411e84e2cb95920b80be114bffc
Owais <owaiswiz@gmail.com>
2018-03-10 11:52:53 +0530

    Make all the required modules global for access on client side

--- encryption-test/index-native.js
+++ encryption-test/index-native.js
@@ -27,26 +27,26 @@
  *
  */
 
-const bip39     = require('bip39');
-const hdkey     = require('ethereumjs-wallet/hdkey');
-const EthCrypto = require('eth-crypto');
-const sha256    = require('js-sha256');
+bip39     = require('bip39');
+hdkey     = require('ethereumjs-wallet/hdkey');
+EthCrypto = require('eth-crypto');
+sha256    = require('js-sha256');
 
-const seed           = bip39.mnemonicToSeed('onion over warrior aware lava crisp hope purpose easy sense purse morning');
-const first_acc_path = "m/44'/60'/0'/0/0";
-const instance       = hdkey.fromMasterSeed(seed);
-const firstAccount   = instance.derivePath(first_acc_path);
+seed           = bip39.mnemonicToSeed('onion over warrior aware lava crisp hope purpose easy sense purse morning');
+first_acc_path = "m/44'/60'/0'/0/0";
+instance       = hdkey.fromMasterSeed(seed);
+firstAccount   = instance.derivePath(first_acc_path);
 
-const privateKey = firstAccount.getWallet().getPrivateKeyString();
-const publicKey  = EthCrypto.publicKeyByPrivateKey(privateKey);
-const address    = EthCrypto.addressByPublicKey(publicKey);
+privateKey = firstAccount.getWallet().getPrivateKeyString();
+publicKey  = EthCrypto.publicKeyByPrivateKey(privateKey);
+address    = EthCrypto.addressByPublicKey(publicKey);
 
 /*
  *  This is the data we get after decryption from the blockchain which is then again encrypted if we need to send it to someone else using
  *  their private key.
  *
  */
-let plainData = {
+plainData = {
   name: 'Owais Khan',
   dob: '30-02-1999',
   aadhar: 'verified',
@@ -69,7 +69,7 @@ plainData = JSON.stringify(plainData);
  *  NOTE: Care should be taken to avoid typos in the key name as well by certain python developers.
  *
  */
-const encryptedData = {
+encryptedData = {
   iv: 'd1bb0dc7e5d0276a33cd5947e5641279',
   ephemPublicKey:'042327160f18c9633b71c98ccf5009218cc2f16e30fc68e8966b804c28c7c6ce85a775e6805763170c1fad07a9af101386f8b8e5196d61888a00ac6285235552fe',
   ciphertext: 'f525d283b6cc44ba05a0f2274af8eb69b7452f883861cd50ff8ece9abba8dd85f3b3f4f15b851899106c0f55c9d75b47aeb9c0bb7c0db9cc32e3a41ef5f32aafed0e6b093db7195fff791dc35e87f33c6a9341390fc394c40b098f1e63804616',
@@ -82,7 +82,7 @@ const encryptedData = {
  *  blockchain (in our case the government)
  *
  */
-const hashStoredOnTheBlockChain = '2ee7e99f148b03ae2a998da1ba2ef667de20e6fa8dc98d19552a586efcf3c503';
+hashStoredOnTheBlockChain = '2ee7e99f148b03ae2a998da1ba2ef667de20e6fa8dc98d19552a586efcf3c503';
 
 /*
  *
@@ -90,7 +90,7 @@ const hashStoredOnTheBlockChain = '2ee7e99f148b03ae2a998da1ba2ef667de20e6fa8dc98
  *  sending
  *
  */
-const signature = EthCrypto.sign(privateKey, plainData);
+signature = EthCrypto.sign(privateKey, plainData);
 console.log('Signature of the message: ', signature);
 
 /*
@@ -99,7 +99,7 @@ console.log('Signature of the message: ', signature);
  *  the block chain. If it's the same, then the sender is the actual owner of the information he sent.
  *
  */
-const addressRecovered = EthCrypto.recover(signature, plainData);
+addressRecovered = EthCrypto.recover(signature, plainData);
 console.log('Address is the same as the address recovered: ', address == addressRecovered)
 
 
@@ -124,3 +124,4 @@ EthCrypto.encryptWithPublicKey(publicKey, plainData)
   .then( (encryptedData) => {
     console.log('Encrypted Data: ',encryptedData);
   });
+
