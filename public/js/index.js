9e2fc66da56ce3807817731bec9e2baf8d653832
Owais <owaiswiz@gmail.com>
2018-03-10 16:12:36 +0530

    Make deploy correct

--- public/js/index.js
+++ public/js/index.js
@@ -9,9 +9,6 @@ web3.eth.defaultAccount = web3.eth.accounts[0];
 // insert abi here
 var abi = abi;
 let CryptoContract = web3.eth.contract(abi);
-
-
-
-let Crypto = CryptoContract.at(address);
+let Crypto = CryptoContract.at(address); 
 
 console.log(Crypto)
